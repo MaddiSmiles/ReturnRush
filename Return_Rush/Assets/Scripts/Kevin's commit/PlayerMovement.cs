6547392bd98ac9a671c2208e21cc9167559ce12b
Kevinlop1141 <146374646+Kevinlop1141@users.noreply.github.com>
2025-06-22 22:07:31 -0400

    added forward dash to player script

--- Return_Rush/Assets/Scripts/Kevin's commit/PlayerMovement.cs
+++ Return_Rush/Assets/Scripts/Kevin's commit/PlayerMovement.cs
@@ -6,6 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    //fields for checking dash
+    [SerializeField] private float dashPower = 10f;
+    //If we want to implement a trail thing
+    //[SerializeField] private TrailRenderer tr;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
     // Start is called before the first frame update
@@ -21,10 +29,45 @@ public class PlayerMovement : MonoBehaviour
     {
         //move object from input
         rb.velocity = moveInput * moveSpeed;
+        //implementing DASH
+        if (Input.GetKeyDown("q") && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+        if (isDashing)
+        {
+            return;
+        }
+    }
+    void FixedUpdate()
+    {
+       if (isDashing)
+        {
+            return;
+        } 
     }
     //Listens for input from player
     public void Move(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue <Vector2>();
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    //setting dash to limit player from spamming
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        //this moves player forward
+        rb.velocity = new Vector2(0f, transform.localScale.y * dashPower);
+        ///trail?
+        /// tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        //tr.emitting = fasle;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
