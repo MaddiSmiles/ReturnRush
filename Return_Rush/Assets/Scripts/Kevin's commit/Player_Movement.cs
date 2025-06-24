eac73fd56d074718d10d130b19b3538bd4c76e23
Kevinlop1141 <146374646+Kevinlop1141@users.noreply.github.com>
2025-06-24 10:16:09 -0400

    made sure my player script was named differently

--- Return_Rush/Assets/Scripts/Kevin's commit/Player_Movement.cs
+++ Return_Rush/Assets/Scripts/Kevin's commit/Player_Movement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class Player_Movement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     //fields for checking dash
