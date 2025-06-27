7b98d6608975d1dca8e25118e6eddff662ee3630
Mschoen03 <mschoen03a@gmail.com>
2025-06-26 19:14:36 -0500

    Created simple main menu

---
+++ Return_Rush/Assets/Scripts/Matt's commit/PlayButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("New Scene"); // Replace with your scene name
+    }
+}
