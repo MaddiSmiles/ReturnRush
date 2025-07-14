82c299872624be04d29b089ca74e6f02348db23e
Kevinlop1141 <146374646+Kevinlop1141@users.noreply.github.com>
2025-07-14 13:12:50 -0400

    Removed text from some buttons

--- Return_Rush/Assets/Scripts/Kevin's commit/Tutorial_triggers.cs
+++ Return_Rush/Assets/Scripts/Kevin's commit/Tutorial_triggers.cs
@@ -5,11 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class Tutorial_triggers : MonoBehaviour
 {
+    public GameObject movement;
+    public GameObject dash;
+    public GameObject enemy_intruct;
+    public GameObject final_words;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("EndZone"))
         {
             SceneManager.LoadScene("Game");
+        }
+        if (other.CompareTag("10"))
+        {
+            movement.SetActive(true);
+        }
+        if (other.CompareTag("30"))
+        {
+            movement.SetActive(true);
+        }
+        if (other.CompareTag("50"))
+        {
+            movement.SetActive(true);
+        }
+        if (other.CompareTag("40"))
+        {
+            movement.SetActive(true);
         }
     }
 }
