2fa67d5af2421723580efe7db670127f947dc685
Kevinlop1141 <146374646+Kevinlop1141@users.noreply.github.com>
2025-07-21 09:34:20 -0400

    Fixed tutorial words

--- Return_Rush/Assets/Scripts/Kevin's commit/text_movement.cs
+++ Return_Rush/Assets/Scripts/Kevin's commit/text_movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class text_movement : MonoBehaviour
@@ -7,15 +8,32 @@ public class text_movement : MonoBehaviour
 
     public float amp;
     Vector3 pos;
+    public PauseMenu pause;
     void Start()
     {
-       pos = transform.position;
-
+        pos = transform.position;
+        
     }
     // Update is called once per frame
     void Update()
     {
+
+        if (pause.isPaused)
+        {
+            pausedGame();
+        }
+        else
+        {
+            moveText();
+        }
+    }
+    void moveText()
+    {
         pos.y = (Mathf.Sin(Time.time) * amp) + pos.y;
         transform.position = pos;
+    }
+    void pausedGame()
+    {
+        return;
     }
 }
